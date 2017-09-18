351f0dbc1629b247a08635f3ac7f59a3060d72bd
kellybarber <kelly.james.barber@gmail.com>
2017-09-18 17:14:35 -0400

    working on receiving broadcasted messages and adding them to the state

--- socket-server/server.js
+++ socket-server/server.js
@@ -1,28 +1,32 @@
 // server.js
 const express      = require('express')
-const SocketServer = require('ws').Server
+const WebSocket    = require('ws').Server
+const uuid         = require('uuid/v4')
+const id           = uuid()
 
-// Set the port to 3001
-const PORT = 3001
-
-// Create a new express server
+const PORT   = 3001
 const server = express()
-   // Make the express server serve static assets (html, javascript, css) from the /public folder
-  .use(express.static('public'))
-  .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`))
+               .use(express.static('public'))
+               .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`))
 
-// Create the WebSockets server
-const wss = new SocketServer({ server })
+const wss = new WebSocket({ server })
+wss.broadcast = (data) => {
+  wss.clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(data)
+    }
+  })
+}
 
-// Set up a callback that will run when a client connects to the server
-// When a client connects they are assigned a socket, represented by
-// the ws parameter in the callback.
+
 wss.on('connection', (ws) => {
   console.log('Client connected')
 
   ws.on('message', (message) => {
     let receivedMessage = JSON.parse(message)
-    console.log(`${receivedMessage.username} said ${receivedMessage.content}`)
+    receivedMessage.id  = id
+
+    wss.broadcast(JSON.stringify(receivedMessage))
   })
 
   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
--- src/App.jsx
+++ src/App.jsx
@@ -7,28 +7,26 @@ class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      currentUser: { name: "Bob" }, 
-      messages: [
-        {
-          id      : 1,
-          username: "Bob",
-          content : "Has anyone seen my marbles?",
-        },
-        {
-          id      : 2,
-          username: "Anonymous",
-          content : "No, I think you lost them. You lost your marbles Bob. You lost them for good."
-        }
-      ]
+      currentUser: { name: "Bob" },
+      messages: []
+    }
+  }
+
+  componentDidMount() {
+    const ws = new WebSocket('ws://localhost:3001')
+    this.ws  = ws
+
+    ws.onopen = (event) => {
+      console.log('Connection Created')
     }
   }
 
   addMessage(content) {
     const newMessage = {
-      id      : Math.random(),
       username: this.state.currentUser.name,
       content : content
     }
+    this.ws.send(JSON.stringify(newMessage))
     const messages = this.state.messages.concat(newMessage)
     this.setState(
       { messages: messages }
